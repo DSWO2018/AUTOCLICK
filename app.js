164548259190b35a83960d79bf39251371feb7e2
Alejandro Gómez <Alejandro>
2018-10-14 20:28:04 -0500

    productos database

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var mongoose = require('mongoose');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -8,11 +11,25 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+mongoose.connect('mongodb://localhost:27017/shopping', {useNewUrlParser: true});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+    secret: 'mysupersecret',
+    resave: false,
+    saveUninitialized : false,
+    store: new MongoStore({mongooseConnection: mongoose.connection}),
+    cookie: {maxAge: 180*60*100}
+}));
+app.use(function (req, res, next) {
+    res.locals.session = req.session;
+    next();
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
